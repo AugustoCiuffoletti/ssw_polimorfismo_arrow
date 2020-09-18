4f48220b73922260bc83c34cef4e6064342a847e
Augusto Ciuffoletti <augusto.ciuffoletti@gmail.com>
2020-09-18 18:33:09 +0200

    eliminata funzione non arrow

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ import './style.css';
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Aprire la consolle per i risultati</h1>`;
 
-function g (x) { return 9*x+2; }
 function zero ( h ) {
   return h(0);
 }
 console.log( zero ( (x) => 4 * x + 3 ) );
-console.log(zero(g));
